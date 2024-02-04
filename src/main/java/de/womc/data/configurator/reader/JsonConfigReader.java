4e429d2d66c0043021675e17ef3a94110a316db7
laubfall <ludwig.daniel@gmail.com>
2024-02-04 13:12:20 +0100

    A new beginning.

---
+++ src/main/java/de/womc/data/configurator/reader/JsonConfigReader.java
@@ -0,0 +1,11 @@
+package de.womc.data.configurator.reader;
+
+import de.womc.data.configurator.BeanDataConfig;
+
+public class JsonConfigReader implements IConfigReader {
+
+	@Override
+	public BeanDataConfig toConfig() {
+		return null;
+	}
+}
